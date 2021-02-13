9e3e79b7f15019f13a40cd6e2efdcee99aa6f466
jeggonlinellc <jeggonlinellc@gmail.com>
2021-02-13 12:09:07 -0800

    destructured arguements

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,14 @@
 import React, { Component, createFactory } from "react";
 
 //Stateless Functional Components
-const NavBar = (props) => {
+//Object Destructuting - adding curly braces to the arguement () and removing "props"
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar{" "}
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
